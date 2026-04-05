45fca9efd1dc07cbb72a32ca13373405f88bd808
Matheus M. de Santana <81048603+MoreiraMatheus@users.noreply.github.com>
2026-04-05 16:19:49 -0300

    revisando design do programa

--- Organizador de Pastas/F_Main.Designer.cs
+++ Organizador de Pastas/F_Main.Designer.cs
@@ -27,39 +27,49 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F_Main));
             btn_selecionar_pasta = new Button();
             tb_caminho = new TextBox();
             btn_organizar = new Button();
             fbd_Bucasdor = new FolderBrowserDialog();
             btn_limpar = new Button();
+            toolTipBotoes = new ToolTip(components);
             SuspendLayout();
             // 
             // btn_selecionar_pasta
             // 
-            btn_selecionar_pasta.Location = new Point(12, 78);
+            btn_selecionar_pasta.BackgroundImage = (Image)resources.GetObject("btn_selecionar_pasta.BackgroundImage");
+            btn_selecionar_pasta.BackgroundImageLayout = ImageLayout.Zoom;
+            btn_selecionar_pasta.Location = new Point(375, 21);
+            btn_selecionar_pasta.Margin = new Padding(4);
             btn_selecionar_pasta.Name = "btn_selecionar_pasta";
-            btn_selecionar_pasta.Size = new Size(111, 23);
-            btn_selecionar_pasta.TabIndex = 0;
-            btn_selecionar_pasta.Text = "Selecionar Pasta";
+            btn_selecionar_pasta.Size = new Size(30, 30);
+            btn_selecionar_pasta.TabIndex = 1;
+            toolTipBotoes.SetToolTip(btn_selecionar_pasta, "Buscar pasta");
             btn_selecionar_pasta.UseVisualStyleBackColor = true;
             btn_selecionar_pasta.Click += btn_selecionar_pasta_Click;
             // 
             // tb_caminho
             // 
             tb_caminho.Enabled = false;
-            tb_caminho.Location = new Point(12, 37);
+            tb_caminho.Font = new Font("Segoe UI", 11F);
+            tb_caminho.Location = new Point(22, 21);
+            tb_caminho.Margin = new Padding(4);
             tb_caminho.Name = "tb_caminho";
             tb_caminho.ReadOnly = true;
-            tb_caminho.Size = new Size(345, 23);
-            tb_caminho.TabIndex = 1;
+            tb_caminho.Size = new Size(345, 27);
+            tb_caminho.TabIndex = 0;
             // 
             // btn_organizar
             // 
-            btn_organizar.Location = new Point(246, 78);
+            btn_organizar.Location = new Point(451, 21);
+            btn_organizar.Margin = new Padding(4);
             btn_organizar.Name = "btn_organizar";
-            btn_organizar.Size = new Size(111, 23);
+            btn_organizar.Size = new Size(100, 30);
             btn_organizar.TabIndex = 3;
             btn_organizar.Text = "Organizar";
+            toolTipBotoes.SetToolTip(btn_organizar, "Organiza os arquivos");
             btn_organizar.UseVisualStyleBackColor = true;
             btn_organizar.Click += btn_organizar_Click;
             // 
@@ -71,11 +81,14 @@
             // 
             // btn_limpar
             // 
-            btn_limpar.Location = new Point(129, 78);
+            btn_limpar.BackgroundImage = (Image)resources.GetObject("btn_limpar.BackgroundImage");
+            btn_limpar.BackgroundImageLayout = ImageLayout.Zoom;
+            btn_limpar.Location = new Point(413, 21);
+            btn_limpar.Margin = new Padding(4);
             btn_limpar.Name = "btn_limpar";
-            btn_limpar.Size = new Size(111, 23);
-            btn_limpar.TabIndex = 4;
-            btn_limpar.Text = "Limpar";
+            btn_limpar.Size = new Size(30, 30);
+            btn_limpar.TabIndex = 2;
+            toolTipBotoes.SetToolTip(btn_limpar, "Limpar pasta");
             btn_limpar.UseVisualStyleBackColor = true;
             btn_limpar.Click += btn_limpar_Click;
             // 
@@ -83,12 +96,17 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(379, 165);
+            ClientSize = new Size(572, 76);
             Controls.Add(btn_limpar);
             Controls.Add(btn_organizar);
             Controls.Add(tb_caminho);
             Controls.Add(btn_selecionar_pasta);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
             Name = "F_Main";
+            Padding = new Padding(8);
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Organizador de Pastas";
             ResumeLayout(false);
             PerformLayout();
@@ -101,5 +119,6 @@
         private Button btn_organizar;
         private FolderBrowserDialog fbd_Bucasdor;
         private Button btn_limpar;
+        private ToolTip toolTipBotoes;
     }
 }
